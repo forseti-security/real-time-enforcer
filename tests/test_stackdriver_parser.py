563ecd89b2df9596b8c917e51a38dc4ccdcfebd4
Artur Fafara <afafa@softserveinc.com>
2019-08-12 10:14:44 +0200

    update test resource method

--- tests/test_stackdriver_parser.py
+++ tests/test_stackdriver_parser.py
@@ -43,7 +43,7 @@ test_single_asset_log_params = [
     ("cloudsql-protoPayload.request.body.json", "sqladmin.instances", "write", "test-instance"),
     ("cloudsql-protoPayload.request.resource.instanceName.instanceId.json", "sqladmin.instances", "write", "test-instance"),
     ("compute-subnetworks-enable-flow-logs.json", "compute.subnetworks", "write", "example"),
-    ("compute-subnetworks-set-private-ip-google-access.json", "compute.subnetworks.privateip", "write", "example"),
+    ("compute-subnetworks-set-private-ip-google-access.json", "compute.subnetworks", "write", "example"),
     ("compute-firewalls-enable-logs-policy.json", "compute.firewalls", "write", "test-firewall")
 
 ]
